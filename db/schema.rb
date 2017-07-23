aa961a70f16a868ffe76c29f03d0f2ae281dc357
jiggypete <jiggypete@gmail.com>
2017-07-23 18:23:46 +0100

    Adding devise :confirmable, along with log in and sign up links

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,13 @@ ActiveRecord::Schema.define(version: 20170723170607) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
