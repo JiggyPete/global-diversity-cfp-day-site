0a1a7581541758b1f7fec5fa17dae4546c8b7a51
jiggypete <jiggypete@gmail.com>
2017-08-19 17:14:39 +0100

    Teach users about their workshop

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+  has_one :workshop, foreign_key: 'organiser_id', class_name: "Workshop"
+
   validates_presence_of :full_name,
                         :biography,
                         :picture_url,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe "associations" do
+    it { should have_one(:workshop)}
+  end
+
   describe "validations" do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:full_name) }
