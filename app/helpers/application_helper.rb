a4cff135e8ada47ccdd2fe5ae23f4f9719039419
jiggypete <jiggypete@gmail.com>
2017-08-24 22:22:16 +0100

    Display full_name / email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def workshop_landing_page_for(user)
     user.workshop.present? ?  workshop_path(user.workshop) : new_workshop_path
   end
+
+  def user_name(user)
+    user.full_name? ? user.full_name : user.email
+  end
 end
