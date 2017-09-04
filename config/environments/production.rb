e861ed647766b28bab99bd00dfc02801b68b7b7d
jiggypete <jiggypete@gmail.com>
2017-09-04 21:59:46 +0100

    Adding prod mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,11 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "global-diversity-cfp-day-site_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: "global-diversity-cfp-day.herokuapp.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'info@globaldiversitycfpday.com'
+  config.mailer_sender = 'peter@globaldiversitycfpday.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
