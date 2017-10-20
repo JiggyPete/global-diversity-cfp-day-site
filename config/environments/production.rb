22ad0bb4153a8f5161de8d2d7873581479be21a1
jiggypete <jiggypete@gmail.com>
2017-10-20 21:38:27 +0100

    Updating prod mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "global-diversity-cfp-day-site_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: "global-diversity-cfp-day.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "globaldiversitycfpday.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
