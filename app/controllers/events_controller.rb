dea3d38df5d9649903562f144554fde56b8d7958
jiggypete <jiggypete@gmail.com>
2017-12-30 16:47:10 +0000

    Removing team member access restriction on event pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,11 +2,6 @@ class EventsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def show
-    workshop = Workshop.find params[:id]
-    if workshop.ticketing_url.present? || current_user.present?
-      @workshop = workshop
-    else
-      raise Exception.new("User trying to access event when tickets are not available")
-    end
+    @workshop = Workshop.find params[:id]
   end
 end
