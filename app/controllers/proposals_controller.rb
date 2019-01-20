003273b1a5ca279884419e2d254a35ede0b6c2c1
jiggypete <jiggypete@gmail.com>
2019-01-20 22:11:59 +0000

    Adding proposal - Overcoming your fear of failure

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -4,6 +4,8 @@ class ProposalsController < ApplicationController
 
   def show
     template = 'how-does-javascript-math'
+    template = params["id"] if params["id"].present?
+
     render template
   end
 end
