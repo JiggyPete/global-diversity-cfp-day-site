0502a0732855b494fe8cc30c9e73e1200a81c636
jiggypete <jiggypete@gmail.com>
2019-02-01 09:47:16 +0000

    Adding flash message when team member has been invited

--- app/controllers/invite_team_members_controller.rb
+++ app/controllers/invite_team_members_controller.rb
@@ -2,6 +2,7 @@ class InviteTeamMembersController < ApplicationController
   def create
     User.invite!(user_params.merge(workshop: current_workshop))
 
+    flash[:notice] = "Invitation has been sent"
     redirect_to workshop_path(current_workshop)
   end
 
