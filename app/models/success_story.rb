418b581bb71c0302f43676176f89bd1cdd3a69cb
jiggypete <jiggypete@gmail.com>
2019-08-23 09:55:18 +0100

    Adding SuccessStory validations

--- app/models/success_story.rb
+++ app/models/success_story.rb
@@ -1,2 +1,15 @@
 class SuccessStory < ApplicationRecord
+
+  validates_presence_of :full_name,
+    :profile_picture_url,
+    :email_address,
+    :talk_title,
+    :event_name,
+    :event_start_date,
+    :event_end_date,
+    :premission_for_public_use,
+    :approved
+
+  validates :profile_picture_url, :url => true
+
 end
