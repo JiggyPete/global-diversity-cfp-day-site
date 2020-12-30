45e563a9a48769587b08d36236f62980408c3041
jiggypete <jiggypete@gmail.com>
2020-12-30 13:55:38 +0000

    Adding I18n to the homepage and CoC page

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module GlobalDiversityCfpDaySite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
